6d6c8292bd31ed8aa4ed1b16c15201eabab16fc2
Shin <pro100pro20021@mail.ru>
2025-03-31 12:57:14 +1000

    controllers1

--- backend/src/controllers/upload.ts
+++ backend/src/controllers/upload.ts
@@ -10,13 +10,15 @@ export const uploadFile = async (
     if (!req.file) {
         return next(new BadRequestError('Файл не загружен'))
     }
+    if (req.file.size < 2 * 1024) {
+        return next(new BadRequestError('Размер файла должен быть > 2KB'))
+    }
     try {
         const fileName = process.env.UPLOAD_PATH
             ? `/${process.env.UPLOAD_PATH}/${req.file.filename}`
             : `/${req.file?.filename}`
         return res.status(constants.HTTP_STATUS_CREATED).send({
             fileName,
-            originalName: req.file?.originalname,
         })
     } catch (error) {
         return next(error)
